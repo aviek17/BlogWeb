cbb012532ac1b4f9543a1e35f4784f46fcfc237f
Aviek <aviek17@gmail.com>
2021-07-10 14:34:50 +0530

    All Post Rendered

---
+++ src/Components/AddPost/Post.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "../AddPost/post.css"
+import Posts from '../Posts/Posts'
+
+const Post = () => {
+    return (
+        <>
+            <div className="posts">
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+                <Posts/>
+            </div>
+        </>
+    )
+}
+
+export default Post
